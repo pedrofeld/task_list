fe760465d6704af495a77d431e4f7ab278bd3b00
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-30 15:32:50 -0300

    feat: adding inputs to add new tasks

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,21 @@
 function AddTask() {
-  return <h1>Add Task</h1>;
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+      <input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        className="border border-slate-300 outline-slate-300 px-4 py-2 rounded-md"
+      />
+      <input
+        type="text"
+        placeholder="Digite o descrição da tarefa"
+        className="border border-slate-300 outline-slate-300 px-4 py-2 rounded-md"
+      />
+      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+        Adicionar
+      </button>
+    </div>
+  );
 }
 
 export default AddTask;
