d558580a7ff3179ea88fdd86f087706c9dd5b407
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-30 13:41:08 -0300

    feat: creating AddTask component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 function App() {
   return (
     <div>
       <h1>Gerenciador de Tarefas</h1>
       <Tasks />
+      <AddTask />
     </div>
   );
 }
