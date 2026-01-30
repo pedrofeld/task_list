11ba82fa2016b4ffc5b6c0795f76bc222ae40d36
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-30 16:06:55 -0300

    feat: adding functionality of adding a new task

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,17 +1,36 @@
-function AddTask() {
+import { useState } from "react";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
   return (
     <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
       <input
         type="text"
         placeholder="Digite o título da tarefa"
         className="border border-slate-300 outline-slate-300 px-4 py-2 rounded-md"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <input
         type="text"
         placeholder="Digite o descrição da tarefa"
         className="border border-slate-300 outline-slate-300 px-4 py-2 rounded-md"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
       />
-      <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">
+      <button
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            return alert("Preencha o título e a descrição da tarefa");
+          }
+          onAddTaskSubmit(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+      >
         Adicionar
       </button>
     </div>
