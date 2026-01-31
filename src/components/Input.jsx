4dab4f6e23c815989e04e9569264bc8599f12046
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-31 10:07:55 -0300

    feat: creating a component to the Input

---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+function Input(props) {
+    return (
+        <input
+            type={props.type}
+            placeholder={props.placeholder}
+            className="border border-slate-300 outline-slate-300 px-4 py-2 rounded-md"
+            value={props.value}
+            onChange={props.onChange}
+        />
+    )
+}
+
+export default Input;
