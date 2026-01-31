17e1b2b93ffcc57706daf83f58174b5644697007
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-31 10:21:05 -0300

    feat: creating a button component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,5 +1,6 @@
 import { ChevronRightIcon, TrashIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom";
+import Button from "./Button"
 
 function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
   const navigate = useNavigate();
@@ -21,18 +22,16 @@ function Tasks({ tasks, onTaskClick, onDeleteTaskClick }) {
           >
             {task.title}
           </button>
-          <button
+          <Button
             onClick={() => onSeeDetailsClick(task)}
-            className="bg-slate-400 p-2 rounded-md text-white"
           >
             <ChevronRightIcon />
-          </button>
-          <button
+          </Button>
+          <Button
             onClick={() => onDeleteTaskClick(task.id)}
-            className="bg-slate-400 p-2 rounded-md text-white"
           >
             <TrashIcon />
-          </button>
+          </Button>
         </li>
       ))}
     </ul>
