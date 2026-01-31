dd766c22e2e00e4d66ae60c3ab3ae36d39d0ab7a
Pedro Feld <pedro.gael.feld@gmail.com>
2026-01-31 10:09:40 -0300

    refactor: improving Input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,8 @@
 function Input(props) {
     return (
         <input
-            type={props.type}
-            placeholder={props.placeholder}
             className="border border-slate-300 outline-slate-300 px-4 py-2 rounded-md"
-            value={props.value}
-            onChange={props.onChange}
+            {...props}
         />
     )
 }
